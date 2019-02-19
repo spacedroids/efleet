aa456ecacfca71536fe735a6df59407a1b264579
Ryan Nordman <spacerobots@gmail.com>
2019-02-18 23:52:34 -0800

    Single bolt on screen shoots up

---
+++ Assets/Scripts/Bolt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    public Rigidbody _rigidBody;
+    public Transform _transform;
+    public float speed = 3;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        _rigidBody.velocity = _transform.up * speed;
+        _rigidBody.angularVelocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
