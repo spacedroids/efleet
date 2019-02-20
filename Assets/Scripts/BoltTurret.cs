90f3349c12c623f7e730cb8b5c61d3fe0aa0fdb1
Ryan Nordman <spacerobots@gmail.com>
2019-02-20 08:55:15 -0800

    Bolt prefab is setup, player script fires via turret battery and turret on mouse click.

---
+++ Assets/Scripts/BoltTurret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltTurret : MonoBehaviour
+{
+    public static GameObject boltprefab;
+
+    void Awake()
+    {
+        boltprefab = (GameObject)Resources.Load("Shots/Bolt");
+    }
+
+    public void fire(Vector3 direction)
+    {
+        GameObject newShot = Object.Instantiate(boltprefab, direction, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TurretBattery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBattery : MonoBehaviour
+{
+    public BoltTurret boltTurret;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fire(Vector3 direction) {
+        boltTurret.fire(direction);
+    }
+}
