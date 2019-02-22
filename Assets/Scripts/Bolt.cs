3f69230417febfabed4018fccaa47d439cd58f80
Ryan Nordman <spacerobots@gmail.com>
2019-02-22 08:57:47 -0800

    Bolts now damage enemy ship. Enemy ship object is removed at health 0

--- Assets/Scripts/Bolt.cs
+++ Assets/Scripts/Bolt.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class Bolt : MonoBehaviour
 {
     public Rigidbody _rigidBody;
-    public Transform _transform;
     public float speed = 3;
 
     void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
-        _transform = GetComponent<Transform>();
     }
 
     void Start()
     {
-        _rigidBody.velocity = _transform.forward * speed;
+        _rigidBody.velocity = transform.forward * speed;
         _rigidBody.angularVelocity = Vector3.zero;
     }
 
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log("bang");
+        Destroy(gameObject);
+        EnemyController damageRecipient = other.gameObject.GetComponent(typeof(EnemyController)) as EnemyController;
+        if(damageRecipient != null)
+        {
+            damageRecipient.Damage(50);
+        }
     }
 
     void Update()
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health = 100;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Damage(int amount) {
+        health -= amount;
+        if(health <= 0) {
+            Kill();    
+        }
+    }
+
+    public void Kill() {
+        Destroy(gameObject);
+    }
+}
